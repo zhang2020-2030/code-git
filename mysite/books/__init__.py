582b6c5b9875ff57331e9389f610b90743ffe4f3
Li xiaolin <510255413@qq.com>
2023-11-28 00:58:40 +0800

    Delete mysite directory

--- mysite/books/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'books.apps.BooksConfig'  # 请替换为您的应用名和配置类名
--- mysite/docs/init.sql
+++
@@ -1,6 +0,0 @@
---
--- Create model Books
---
-CREATE TABLE `books_books` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `activity` varchar(128) NOT NULL, `type` varchar(32) NOT NULL, `time` varchar(32) NOT NULL, `cit
-y` varchar(32) NOT NULL, `addr` varchar(128) NOT NULL, `p_number` varchar(128) NOT NULL);
-
